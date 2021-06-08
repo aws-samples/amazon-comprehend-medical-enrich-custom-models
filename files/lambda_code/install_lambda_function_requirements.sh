fb5df405a81ae718009ecb4bead9f6f0598880ab
Broyde <broydj@freeip.amazon.com>
2021-06-08 11:38:07 -0400

    add the readme and instructions

---
+++ files/lambda_code/install_lambda_function_requirements.sh
@@ -0,0 +1,4 @@
+#this script installs the relevant dependencies for the lambda function locally and zips it with the lambda function
+rm -rf package # remove the package directory if it exists already
+cat requirements.txt |while read line; do pip install --target ./package $line; done
+cd package/;zip -r9 ${OLDPWD}/lambda.zip .;cd ..;zip -g lambda.zip  call_cm_other_models_lambda.py;
